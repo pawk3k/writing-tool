947bf82d1e90bf7a3259c1ad90600a9f2255488d
Pavlo-Bohdan Ravliv <ravliv.pavlobohdan@gmail.com>
2025-03-20 09:17:04 +0100

    remove unused thing

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   extends: [
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:depend/recommended",
     "prettier",
     "plugin:@typescript-eslint/eslint-recommended",
   ],
