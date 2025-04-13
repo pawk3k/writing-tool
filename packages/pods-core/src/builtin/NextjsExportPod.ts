c76e96a162af6e33963d8bc090c3aa15da32e3a5
Pavlo-Bohdan Ravliv <ravliv.pavlobohdan@gmail.com>
2025-04-13 10:16:38 +0200

    verify statically that everything works

--- packages/pods-core/src/builtin/NextjsExportPod.ts
+++ packages/pods-core/src/builtin/NextjsExportPod.ts
@@ -218,6 +218,7 @@ export class NextjsExportPodUtils {
       out = await $$(cmd, { cwd: nextPath });
     } else {
       out = $$(cmd, { cwd: nextPath });
+      // @ts-expect-error error
       out.stdout?.pipe(process.stdout);
     }
     return out;
@@ -231,6 +232,7 @@ export class NextjsExportPodUtils {
     const { nextPath, quiet, windowsHide } = opts;
     const cmdDev = quiet ? "npm run --silent dev" : "npm run dev";
     const out = $$(cmdDev, { cwd: nextPath, windowsHide });
+    // @ts-expect-error error
     out.stdout?.pipe(process.stdout);
     return out.pid;
   }
