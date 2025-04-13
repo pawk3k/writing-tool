3d6125613715fb02c2f973fea38a6df8a3c94708
Pavlo-Bohdan Ravliv <ravliv.pavlobohdan@gmail.com>
2025-04-13 09:56:04 +0200

    supress the errors to get clean build

--- vendor/dendron-remark-math/types/test.ts
+++ vendor/dendron-remark-math/types/test.ts
@@ -6,6 +6,8 @@ unified().use(math)
 // $ExpectType Processor<Settings>
 unified().use(math, {inlineMathDouble: true})
 // $ExpectError
+// @ts-expect-error TS2322 - Type 'number' is not assignable to type 'boolean | undefined'.
 unified().use(math, {inlineMathDouble: 3})
 // $ExpectError
+// @ts-expect-error TS2353 - Object literal may only specify known properties, and 'invalidProp' does not exist in type 'RemarkMathOptions'.
 unified().use(math, {invalidProp: true})
